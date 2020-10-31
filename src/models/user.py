cc1e8e38fe97ed22be7df1fd6da08e7e1fa0f68f
Mario Briseño <mario.briseno7@gmail.com>
2020-10-30 21:54:05 -0600

    Forgot to add queriable columns to User model

--- src/models/user.py
+++ src/models/user.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
@@ -8,3 +8,19 @@ class User:
     id: int
     issuers: list
     cash: Decimal
+
+    pk: str = field(
+        init=False,
+        metadata={
+            'name': 'Partition Key',
+            'type': 'hash',
+            'hashed': False,
+            'structure': '{id}'
+        }
+    )
+
+    def __post_init__(self):
+        """Creates Partition and Sort keys checks data integrity against entity constraints."""
+
+        # Composite Primary key creation
+        setattr(self, 'pk', f"user#{self.id}")
