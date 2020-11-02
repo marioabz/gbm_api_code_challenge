1ee7a4d1a8433b3f91c0a43a9686e1fdf7538c25
Mario Briseño <mario.briseno7@gmail.com>
2020-11-01 21:59:28 -0600

    Adding transaction to register_user so that no soecial cases are being handled after API creation

--- src/app.py
+++ src/app.py
@@ -1,15 +1,26 @@
 import flask
+import time
 from flask import Flask, request
-from .models import User
+from .models import User, Transaction
 from .errors import get_response_body
 from decimal import Decimal
 from botocore.exceptions import ClientError
-from .queries import Queries
+from .custom_exceptions import (
+    NoStockError,
+    TimestampError,
+    NegativeValuesError,
+    ServiceNotAvailable,
+    NotEnoughBalanceError,
+    InvalidOperationError,
+    SameTransactionException,)
+from datetime import datetime
+from .queries import Queries, RedisDB
 from .serializers import parse_user
 
 registered_users = 0
 app = Flask(__name__)
 connector = Queries()
+cache = RedisDB()
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -20,22 +31,44 @@ def index():
     )
 
 
-@app.route("/accounts", methods=["GET", "POST"])
+@app.route("/accounts", methods=["POST"])
 def register_user():
 
     try:
         _cash = request.form["cash"]
         last_id = connector.get_last_user()
+        _id = last_id + 1
         new_user = User(
             cash=_cash,
-            id=last_id + 1
+            id=_id
         )
         connector.insert_item(new_user)
+
+        new_transaction = Transaction(
+            id=_id,
+            operation="account_created",
+            issuer_name="",
+            share_price=Decimal("0"),
+            total_shares=Decimal("0"),
+            user_shares=Decimal(0),
+            change_cash=Decimal("0"),
+            change_shares=Decimal("0"),
+            time=str(int(time.time())),
+            user_balance=_cash,
+        )
+        connector.insert_item(new_transaction)
         return flask.jsonify(
             get_response_body(
                 code=200,
                 data=parse_user(new_user.__dict__),
                 message=""
             ))
-    except ClientError:
-        return
+
+    # Something went wrong when calling DynamoDB
+    except ClientError as e:
+        return flask.jsonify(
+            get_response_body(
+                code=400,
+                data=dict(),
+                message=e
+            ))
