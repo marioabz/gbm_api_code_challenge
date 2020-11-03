e4c1bada62257554d50287dac14b4afb561a38e9
Mario Briseño <mario.briseno7@gmail.com>
2020-11-03 03:17:21 -0600

    Handled the returning of http codes as flask should do it

--- src/app.py
+++ src/app.py
@@ -28,7 +28,7 @@ def index():
 
     return flask.jsonify(
         ["Welcome to GBM API code challenge"]
-    )
+    ), 200
 
 
 @app.route("/accounts", methods=["POST"])
@@ -57,21 +57,23 @@ def register_user():
             user_balance=_cash,
         )
         connector.insert_item(new_transaction)
+        http_code = 200
         return flask.jsonify(
             get_response_body(
-                code=200,
+                code=http_code,
                 data=parse_user(new_user.__dict__),
                 message=""
-            ))
+            )), http_code
 
     # Something went wrong when calling DynamoDB
     except ClientError as e:
+        http_code = 400
         return flask.jsonify(
             get_response_body(
-                code=400,
+                code=http_code,
                 data=dict(),
                 message=e
-            ))
+            )), http_code
 
 
 @app.route("/accounts/<int:_id>/orders", methods=["POST"])
@@ -112,13 +114,13 @@ def make_transaction(_id):
         )
 
         if not is_transaction_repeated:
+            http_code = 400
             return flask.jsonify(
                 get_response_body(
-                    code=400,
+                    code=http_code,
                     data=dict(),
                     message="Something went wrong"
-                )
-            )
+                )), http_code
 
         # Check if timestamp is greater than now()
         if timestamp >= time_now:
@@ -172,9 +174,10 @@ def make_transaction(_id):
 
         connector.insert_item(_tx)
         user_transactions = connector.get_all_transactions_from_user(_id, time_now)
+        http_code = 200
         return flask.jsonify(
             get_response_body(
-                code=200,
+                code=http_code,
                 data={
                     "current_balance": {
                         "cash": str(arg_user_balance),
@@ -182,92 +185,105 @@ def make_transaction(_id):
                     }
                 },
                 message=""
-            ))
+            )), http_code
 
     # Catch incomplete requests
     except (KeyError, ClientError) as e:
+        http_code = 400
         return flask.jsonify(
             get_response_body(
-                code=400,
+                code=http_code,
                 data=dict(),
                 message=e
-            ))
+            )), http_code
 
     # Transaction already made in the last 5 minutes
     except SameTransactionException as e:
+        http_code = 425
         return flask.jsonify(
             get_response_body(
-                code=425,
+                code=http_code,
                 data=dict(),
                 message="Duplicate transactions are "
                         "not allowed"
-            ))
+            )), http_code
 
     # Catch trade where user intends to sell without owning stocks
     except NoStockError:
+        http_code = 403
         return flask.jsonify(
             get_response_body(
-                code=403,
+                code=http_code,
                 data=dict(),
                 message="You must own the stock "
                         "you are trying to sell"
-            ))
+            )), http_code
 
     # Catch errors when user does not have balance
     except NotEnoughBalanceError:
+        http_code = 403
         return flask.jsonify(
             get_response_body(
-                code=403,
+                code=http_code,
                 data=dict(),
                 message="Your balance is not enough to complete "
                         "this transaction"
-            ))
+            )), http_code
 
     # Catch case where user is intending to request trade out of operational time
     except ServiceNotAvailable:
+        http_code = 403
         return flask.jsonify(
             get_response_body(
-                code=403,
+                code=http_code,
                 data=dict(),
                 message="Service is not available right now. Retry "
                         "between a period of time of 6am and 3pm"
-            ))
+            )), http_code
 
     except TimestampError:
+        http_code = 400
         return flask.jsonify(
             get_response_body(
-                code=400,
+                code=http_code,
                 data=dict(),
                 message="Timestamp value is wrong. Use lower value "
                         "than now AND greater than your last transaction"
-            )
-        )
+            )), http_code
 
     # Data could not be caster correctly
     except NegativeValuesError:
+        http_code = 400
         return flask.jsonify(
             get_response_body(
-                code=400,
+                code=http_code,
                 data=dict(),
                 message="Price or amount of shares should not "
                         "be negative or zero"
-            ))
+            )), http_code
 
     # Transaction went wrong at database layer
     except ClientError:
+        http_code = 400
         return flask.jsonify(
             get_response_body(
-                code=400,
+                code=http_code,
                 data=dict(),
                 message="Something went wrong with this operation. Please try again later"
-            ))
+            )), http_code
 
     # Data could not be caster correctly
     except Exception as e:
+        http_code = 400
         return flask.jsonify(
             get_response_body(
-                code=400,
+                code=http_code,
                 data=dict(),
                 message="Some parameters of request body are wrong. "
                         "Please check your parameters"
-            ))
+            )), http_code
+
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=True, port=80)
